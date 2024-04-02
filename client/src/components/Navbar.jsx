e37f5f271d98903fbb0f91c35b85e0ce7ae2d939
salidamaharjan <salidamaharjan@outlook.com>
2024-04-01 23:47:11 -0500

    Add mock pages to check functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,33 @@
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 export default function NavBar() {
-    return<nav className="flex flex-row gap-2 px-4 justify-end">
-        <a href="/home">Home</a>
-        <a href="">Profile</a>
+  const location = useLocation();
+  const { pathname } = location;
+  return (
+    <nav>
+      <ul className="flex flex-row gap-2 px-4 justify-end">
+        <NavItem
+          className={pathname === "/home" ? "bg-blue-500" : ""}
+          to="/home"
+        >
+          Home
+        </NavItem>
+        <NavItem
+          className={pathname === "/profile" ? "bg-blue-500" : ""}
+          to="/profile"
+        >
+          Profile
+        </NavItem>
+      </ul>
     </nav>
+  );
+}
+function NavItem({ to, children, className }) {
+  return (
+    <li
+      className={`hover:underline hover:underline-offset-4 font-bold px-2 rounded-md ${className}`}
+    >
+      <Link to={to}>{children}</Link>
+    </li>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,22 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-        {
-            path: "/home",
-            element: <Home/>
-        }
-    ]
-  }
-  
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
