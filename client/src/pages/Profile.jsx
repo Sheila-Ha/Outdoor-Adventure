f5f1f8b36891bb2f139099c79fed7ad22a1cc130
salidamaharjan <salidamaharjan@outlook.com>
2024-04-02 11:58:32 -0500

    Fetch data to show to profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Profile() {
-  return <div className="text-xl font-bold">Profile Page</div>;
+  const [allUser, setAllUser] = useState([]);
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  async function fetchUser() {
+    const userData = await fetch("http://localhost:3000/api/users", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const response = await userData.json();
+    setAllUser(response);
+  }
+
+  return (
+    <div>
+      <div className="text-xl font-bold">Profile Page</div>
+      <div>
+        {allUser.map((user) => {
+          return <div key={user}>{user.name}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = require("./routes");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
-app.use(router);
 app.use(express.json());
+app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
