2b3df9a73652987a05a547b07c3568c50058ca33
salidamaharjan <salidamaharjan@outlook.com>
2024-04-02 16:35:31 -0500

    Create folder for Query and Mutation and moved user query

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,8 @@
-import { useQuery, gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { FIND_USERS } from "../graphql/query";
 
-const Find_Users = gql`
-  query FindUsers {
-    findUsers {
-      id
-      name
-      interest
-    }
-  }
-`;
 export default function Profile() {
-  const { loading, error, data } = useQuery(Find_Users);
+  const { loading, error, data } = useQuery(FIND_USERS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
