26e3cea10f525197c38722e23ad83c7712865157
salidamaharjan <salidamaharjan@outlook.com>
2024-04-03 12:15:13 -0500

    make login page ui responsive

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -7,6 +7,15 @@ import { Label } from "../components/Label.jsx";
 export default function LoginPage() {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="bg-muted  lg:block">
+        <img
+          src="/images/imglanding.jpeg"
+          alt="Image"
+          //   width="1920"
+          //   height="1080"
+          className="md:h-full md:w-full max-h-[250px] min-w-[100%] object-fill lg:max-h-full dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
@@ -52,15 +61,6 @@ export default function LoginPage() {
             </Link>
           </div>
         </div>
-      </div>
-      <div className="hidden bg-muted lg:block">
-        <img
-          src="/images/imglanding.jpeg"
-          alt="Image"
-        //   width="1920"
-        //   height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
       </div>
     </div>
   );
