6665dbe52fb0ec7289ad5dc8746412061c9d2923
salidamaharjan <salidamaharjan@outlook.com>
2024-04-03 12:47:04 -0500

    Update CSS for landing page

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,13 +1,7 @@
 import { Link } from "react-router-dom";
 
 import { Button } from "../components/Button.jsx";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../components/Card.jsx";
+
 import { Input } from "../components/Input.jsx";
 import { Label } from "../components/Label.jsx";
 
@@ -16,7 +10,7 @@ export default function SignUpPage() {
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="bg-muted  lg:block">
         <img
-          src="/images/imglanding.jpeg"
+          src="/images/signup.jpeg"
           alt="Image"
           //   width="1920"
           //   height="1080"
@@ -24,50 +18,46 @@ export default function SignUpPage() {
         />
       </div>
       <div className="flex items-center justify-center py-12">
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-xl">Sign Up</CardTitle>
-            <CardDescription>
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Sign Up</h1>
+            <p className="text-balance text-muted-foreground">
               Enter your information to create an account
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-4">
-              <div className="grid grid-cols-2 gap-4">
-                <div className="grid gap-2">
-                  <Label htmlFor="first-name">First name</Label>
-                  <Input id="first-name" placeholder="Max" required />
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="last-name">Last name</Label>
-                  <Input id="last-name" placeholder="Robinson" required />
-                </div>
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
-              </div>
-              <Button type="submit" className="w-full">
-                Create an account
-              </Button>
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="first-name">First name</Label>
+              <Input id="first-name" placeholder="Max" required />
             </div>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?{" "}
-              <Link to={"/login"} className="underline">
-                Sign in
-              </Link>
+            <div className="grid gap-2">
+              <Label htmlFor="last-name">Last name</Label>
+              <Input id="last-name" placeholder="Robinson" required />
             </div>
-          </CardContent>
-        </Card>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button type="submit" className="w-full">
+            Create an account
+          </Button>
+          <div className="grid gap-2 mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <Link to={"/login"} className="underline">
+            Sign in
+          </Link>
+        </div>
+        </div>
       </div>
     </div>
   );
