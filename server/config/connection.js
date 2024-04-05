dc4a73fdc61289684a7b590fd632c8a4f975782b
salidamaharjan <salidamaharjan@outlook.com>
2024-04-05 13:00:16 -0500

    Install Sequelize and Create sequelize connection using postgresSQL

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import Sequelize from 'sequelize';
+
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.USERNAME, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'postgres' 
+});
+
+export default sequelize;
