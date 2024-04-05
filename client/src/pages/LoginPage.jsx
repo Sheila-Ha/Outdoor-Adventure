ae6bfe10933cefa7f937f58c4f11723e4e90e147
salidamaharjan <salidamaharjan@outlook.com>
2024-04-05 10:38:08 -0500

    Retrieve JWT token for login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -24,6 +24,7 @@ export default function LoginPage() {
           ...user,
         },
       });
+      localStorage.setItem("token", data.login);
       return data;
     } catch (error) {
       console.log(error);
