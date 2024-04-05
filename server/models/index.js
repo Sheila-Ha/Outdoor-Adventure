4986c4cf0012b5dcf4f1047b8d925ee84b1844cf
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-04-05 00:05:48 -0500

    restructured database per group conversation and made changes to sequelize

--- server/models/index.js
+++ server/models/index.js
@@ -1,17 +1,48 @@
 const User = require('./User');
 const ExploreLevel = require('./ExploreLevel');
+const CurrentMission = require('./CurrentMission');
+const Category = require('./Category');
+const Activity = require('./Activity');
 
-// Define a User as having one ExploreLevel to create a foreign key in the `ExploreLevel` table
+// one to one User has one ExploreLevel
 User.hasOne(ExploreLevel, {
   foreignKey: 'userId',
-  // When we delete a User, make sure to also delete the associated ExploreLevel.
   onDelete: 'CASCADE',
 });
 
-// We can also define the association starting with ExploreLevel
 ExploreLevel.belongsTo(User, {
   foreignKey: 'userId',
 });
 
-// We package our two models and export them as an object so we can import them together and use their proper names
-module.exports = { User, ExploreLevel };
+// one to one User has one CurrentMission
+User.hasOne(CurrentMission, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE',
+});
+
+CurrentMission.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+// one to one CurrentMission has one Category
+CurrentMission.hasOne(Category, {
+  foreignKey: 'currentMissionId',
+  onDelete: 'CASCADE',
+});
+
+Category.belongsTo(CurrentMission, {
+  foreignKey: 'currentMissionId',
+});
+
+// one to many CurrentMission has many Activity
+CurrentMission.hasMany(Activity, {
+  foreignKey: 'currentMissionId',
+  onDelete: 'CASCADE',
+  });
+
+  Activity.belongsTo(CurrentMission, {
+    foreignKey: 'currentMissionId',
+  });
+
+// We package our models and export them as an object so we can import them together and use their proper names
+module.exports = { User, ExploreLevel, CurrentMission, Category, Activity };
