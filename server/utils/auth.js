66b64922146b6632c94ea663cb17644c9e6cbee6
salidamaharjan <salidamaharjan@outlook.com>
2024-04-04 22:31:25 -0500

    Add auth.js and authMiddleware

---
+++ server/utils/auth.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config();
+
+export function authMiddleware({ req }) {
+  let token = req.headers.authorization;
+  if (req.headers.authorization) {
+    token = token.split(" ").pop().trim();
+  }
+  if (!token) {
+    return req;
+  }
+  try {
+    const { data } = jwt.verify(token, process.env.SECRET_KEY, {
+      maxAge: 60 * 60 * 2,
+    });
+    req.user = data;
+  } catch {
+    console.log("Invalid token");
+  }
+
+  return req;
+}
