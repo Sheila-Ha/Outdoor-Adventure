4729fb71f3558b09b97650c7bedd5757978e418a
salidamaharjan <salidamaharjan@outlook.com>
2024-04-11 10:24:30 -0500

    fix .env variable property

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME, 'postgres', 'HRV4Lexus!', {
-  host: process.env.HOST,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'postgres' 
 });
 
