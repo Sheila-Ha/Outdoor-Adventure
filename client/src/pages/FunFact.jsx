2e79f70e6d944c037316366f53c0d5f3f297894b
salidamaharjan <salidamaharjan@outlook.com>
2024-04-11 20:11:39 -0500

    Add AlerDialog in funfact page

--- client/src/pages/FunFact.jsx
+++ client/src/pages/FunFact.jsx
@@ -1,5 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { FUN_FACT } from "../graphql/query";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../components/Alertdialog.jsx";
 
 export default function FunFact() {
   const { loading, error, data } = useQuery(FUN_FACT);
@@ -7,9 +18,28 @@ export default function FunFact() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   return (
-    <div className="p-6">
-       <h1 className="font-bold">{data.funFact.title}</h1>
-       <p>{data.funFact.description}</p>
+    <div className="p-4">
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <div className="border-2 px-2 rounded-md">Open</div>
+        </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{data.funFact.title}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {data.funFact.description}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Done</AlertDialogCancel>
+          <AlertDialogAction>Learn More</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
     </div>
+    // <div className="p-6">
+    //    <h1 className="font-bold">{data.funFact.title}</h1>
+    //    <p>{data.funFact.description}</p>
+    // </div>
   );
 }
