dfa97f8290050e4006100efcd7d67a677c2104f1
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-04-13 16:51:40 -0500

    remove my hardcoded values

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME, "postgres", "HRV4Lexus!", {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.HOST,
   dialect: 'postgres' 
 });
