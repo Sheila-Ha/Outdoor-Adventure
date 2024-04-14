e86b7e07b2f330cea8bdba6e0c21eb17affb6cb5
salidamaharjan <salidamaharjan@outlook.com>
2024-04-14 12:08:21 -0500

    Install lucide react

--- client/src/components/FunFact.jsx
+++ client/src/components/FunFact.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { FUN_FACT } from "../graphql/query/index.js";
+import { Alert, AlertDescription, AlertTitle } from "./Alert.jsx";
+import { Terminal } from "lucide-react"
 
 // export default function FunFact() {
 //   const { loading, error, data } = useQuery(FUN_FACT);
@@ -18,8 +20,18 @@ export default function FunFact() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   return (
-    <div className="p-2 text-center bg-green-300">
-      <p className="text-sm">{data.funFact.fact}</p>
+    <div>
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
+
+    // <div className="p-2 text-center bg-green-300">
+    //   <p className="text-sm">{data.funFact.fact}</p>
+    // </div>
   );
 }
