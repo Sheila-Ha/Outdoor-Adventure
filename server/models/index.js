ad10c92117e82a0702176e1a8b9ce81cb68d6460
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-04-16 10:42:45 -0500

    added explore level and mission types seeds for final game; added join var to many to many in index.js

--- server/models/index.js
+++ server/models/index.js
@@ -69,8 +69,10 @@ Activities.hasMany(Current_Mission, {
  as: 'activityId'
 });
 */
-Current_Mission.belongsToMany(Activities, { through: Mission_Activities });
-Activities.belongsToMany(Current_Mission, { through: Mission_Activities });
+//Current_Mission.belongsToMany(Activities, { through: Mission_Activities });
+//Activities.belongsToMany(Current_Mission, { through: Mission_Activities });
+Current_Mission.belongsToMany(Activities, { as: 'activityId', through: 'Mission_Activities' });
+Activities.belongsToMany(Current_Mission, { as: 'missionId', through: 'Mission_Activities' });
 
 //
 // We package our models and export them as an object so we can import them together and use their proper names
