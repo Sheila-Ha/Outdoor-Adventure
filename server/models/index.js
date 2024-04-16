4669c1ce61d2155c2e646b9ce874a54cad7fd624
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-04-15 19:40:39 -0500

    possible fix for many to many in index.js

--- server/models/index.js
+++ server/models/index.js
@@ -53,8 +53,10 @@ Activities.belongsTo(Mission_Types, {
 
 // CurrentMission connection with Activities
 // CurrentMission has many Activities AND Activities has many CurrentMissions
-Current_Mission.belongsToMany(Activities,{through:Mission_Activities});
-Activities.belongsToMany(Current_Mission, {through: Mission_Activities});
+//Current_Mission.belongsToMany(Activities,{through:Mission_Activities});
+//Activities.belongsToMany(Current_Mission, {through: Mission_Activities});
+Current_Mission.belongsToMany(Activities,{as: 'activities', through:'Mission_Activities'});
+Activities.belongsToMany(Current_Mission, {as: 'current_mission', through: 'Mission_Activities'});
 
 //
 // We package our models and export them as an object so we can import them together and use their proper names
